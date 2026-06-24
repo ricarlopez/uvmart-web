a7eb25fece08097c9cb17fe3731b9ca57850b725
uvmart Dev Team <dev@uvmart.mx>
2026-06-24 09:40:12 -0600

    refactor(app): extraer constantes a modulo de configuracion

---
+++ config.js
@@ -0,0 +1,9 @@
+// uvmart — Configuracion global
+const CONFIG = {
+  impuesto:                0.16,
+  descuentoVolumen:        0.10,
+  cantidadMinimaDescuento: 5,
+  cantidadMaxima:          99,
+  moneda:                  "MXN",
+  locale:                  "es-MX",
+};
